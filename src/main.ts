452f5fc22062b69c2350a24b058419381618179f
Sergio Martin <smartinsantos@gmail.com>
2022-12-01 11:06:43 -0600

    added timeout global interceptor

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from 'src/common/filters/http-exception/http-exception.filter';
 import { WrapResponseInterceptor } from 'src/common/interceptors/wrap-response/wrap-response.interceptor';
+import { TimeoutInterceptor } from 'src/common/interceptors/timeout/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -22,7 +23,10 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter());
 
   // global interceptor to wrap responses
-  app.useGlobalInterceptors(new WrapResponseInterceptor());
+  app.useGlobalInterceptors(
+    new WrapResponseInterceptor(),
+    new TimeoutInterceptor(),
+  );
 
   await app.listen(3000);
 }
